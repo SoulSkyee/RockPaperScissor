6b380165fc3f8ec7a7c0236c17b716ccc1742a61
SoulSkyee <benjiminfrinklin@gmail.com>
2025-12-17 12:21:43 +0700

    add final code

--- index.js
+++ index.js
@@ -1,83 +1,165 @@
+let humanScore = 0;
+let computerScore = 0;
+
+const rockBtn = document.querySelector(".rock");
+const paperBtn = document.querySelector(".paper");
+const scissorsBtn = document.querySelector(".scissors");
+const resultDiv = document.querySelector(".result");
+
+// bikin 2 div satu untuk hasil, satu untuk ronde.
+const roundDiv = document.createElement("div");
+roundDiv.classList.add("round");
+
+const scoreDiv = document.createElement("div");
+scoreDiv.classList.add("score");
+
+// bikin winner Div
+const winnerDiv = document.createElement("div");
+winnerDiv.classList.add("winner");
+
+resultDiv.textContent = "";
+resultDiv.appendChild(roundDiv);
+resultDiv.appendChild(scoreDiv);
+resultDiv.appendChild(winnerDiv);
+
+// bikin function untuk ngehandle pilihan
+function handleClick(choice) {
+     if (humanScore === 5 || computerScore === 5) return;
+     const result = playRound(choice);
+     // buat ngurusin pilihan user dan komputer
+     roundDiv.textContent = `Kamu milih ${result[0]} dan komputer milih ${result[1]} hasilnya adalah ${result[2]}`;
+     // buat ngurusin hasil round
+     scoreDiv.textContent = `Skor kamu ${humanScore} dan Skor komputer ${computerScore}`;
+     // buat kasih ronde
+     if (humanScore === 5 || computerScore === 5) {
+          if (humanScore === 5) {
+               winnerDiv.textContent = "Kamu menang game!"
+          } else if (computerScore === 5) {
+               winnerDiv.textContent = "Komputer menang game!"
+          }
+
+           disableButton();
+     }
+}
+
+function disableButton() {
+     rockBtn.disabled = true;
+     paperBtn.disabled = true;
+     scissorsBtn.disabled = true;
+}
+
+// masukkin event listener
+rockBtn.addEventListener("click", () => {
+     handleClick("rock");
+});
+paperBtn.addEventListener("click", () => {
+     handleClick("paper");
+});
+scissorsBtn.addEventListener("click", () => {
+     handleClick("scissors");
+});
+
+
 function getComputerChoice() {
      const random = Math.random();
-     if (random < 0.33) return "rock";
-     else if (random < 0.66) return "paper";
+     if (random < 1 / 3) return "rock";
+     else if (random < 2 / 3) return "paper";
      else return "scissors";
 }
 
-function getHumanChoice() {
-     while (true) {
-          const input = prompt("Masukkan Pilihanmu (rock, paper, scissors):").toLowerCase();
+// function getHumanResult() {
+//      while (true) {
+//           const input = prompt("Masukkan Pilihanmu (rock, paper, scissors):").toLowerCase();
 
-          if (input === "rock" || input === "paper" || input === "scissors") {
-               return input;
-          }
+//           if (input === "rock" || input === "paper" || input === "scissors") {
+//                return input;
+//           }
 
-          if (input === null) {
-               console.log("cancelled");
-               return null;
-          }
+//           if (input === null) {
+//                console.log("cancelled");
+//                return null;
+//           }
 
-          console.log("Not Valid! Try again. ");
+//           console.log("Not Valid! Try again. ");
+//      }
+// }
+
+function playRound(playerSelection) {
+     const human = playerSelection;
+     const computer = getComputerChoice();
+
+     if (human === computer) {
+          return [human, computer, "Draw!"];
      }
+
+     if (human === "rock" && computer === "scissors" ||
+          human === "paper" && computer === "rock" ||
+          human === "scissors" && computer === "paper"
+     ) {
+          humanScore++;
+          return [human, computer, "Win!"];
+     }
+     computerScore++;
+     return [human, computer, "Lose!"];
 }
 
-function playGame() {
 
-     let humanScore = 0;
-     let computerScore = 0;
 
-     // playRound DIPINDAH ke dalam playGame
-     function playRound() {
-          const human = getHumanChoice();
-          const computer = getComputerChoice();
+// function playGame() {
 
-          console.log(`You pick: ${human}`);
-          console.log(`Computer picks: ${computer}`);
+//      let humanScore = 0;
+//      let computerScore = 0;
 
-          if (human === computer) {
-               console.log("DRAW!");
-               return "draw";
-          }
+//      // playRound DIPINDAH ke dalam playGame
+//      // function playRound() {
+//      //      const human = getHumanResult();
+//      //      const computer = getComputerResult();
 
-          if (
-               (human === "rock" && computer === "scissors") ||
-               (human === "paper" && computer === "rock") ||
-               (human === "scissors" && computer === "paper")
-          ) {
-               console.log("You win the round!");
-               return "win";
-          }
+//      //      console.log(`You pick: ${human}`);
+//      //      console.log(`Computer picks: ${computer}`);
 
-          console.log("You lose the round!");
-          return "lose";
-     }
+//      //      if (human === computer) {
+//      //           console.log("DRAW!");
+//      //           return "draw";
+//      //      }
 
-     // Main 5 ronde
-     for (let i = 0; i < 5; i++) {
-          const result = playRound();
+//      //      if (
+//      //           (human === "rock" && computer === "scissors") ||
+//      //           (human === "paper" && computer === "rock") ||
+//      //           (human === "scissors" && computer === "paper")
+//      //      ) {
+//      //           console.log("You win the round!");
+//      //           return "win";
+//      //      }
 
-          if (result === "win") {
-               humanScore++;
-          }
+//      //      console.log("You lose the round!");
+//      //      return "lose";
+//      // }
 
-          if (result === "lose") {
-               computerScore++;
-          }
+//      // Main 5 ronde
+//      for (let i = 0; i < 5; i++) {
+//           const result = playRound();
 
-          console.log(`Score: You ${humanScore} - ${computerScore} Computer`);
-          console.log("--------------------------");
-     }
+//           if (result === "win") {
+//                humanScore++;
+//           }
 
-     console.log(`===FINAL RESULT===`);
-     if (humanScore > computerScore) {
-          console.log(`You Win The Game!`);
-     } else if (humanScore < computerScore) {
-          console.log(`Computer Win The Game!`);
-     }else{
-          console.log(`Both of You Draw!`);
-     }
-}
+//           if (result === "lose") {
+//                computerScore++;
+//           }
 
-playGame();
- 
+//           console.log(`Score: You ${humanScore} - ${computerScore} Computer`);
+//           console.log("--------------------------");
+//      }
+
+//      console.log(`===FINAL RESULT===`);
+//      if (humanScore > computerScore) {
+//           console.log(`You Win The Game!`);
+//      } else if (humanScore < computerScore) {
+//           console.log(`Computer Win The Game!`);
+//      } else {
+//           console.log(`Both of You Draw!`);
+//      }
+// }
+
+// playGame();
